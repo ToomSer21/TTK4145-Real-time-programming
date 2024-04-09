ab8565772b91f32668c7918b94667717bb045bf9
ToomSer21 <117809894+ToomSer21@users.noreply.github.com>
2024-04-09 08:58:14 +0200

    Update udp_receiver.c

--- ex2/udp_receiver.c
+++ ex2/udp_receiver.c
@@ -5,6 +5,7 @@
 #include <sys/socket.h>
 
 #define PORT 30000
+#define MAXLINE 1024
 
 int main() {
 
@@ -12,9 +13,9 @@ int main() {
   int sockfd;
   
   if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
-		perror("socket creation failed");
-		exit(EXIT_FAILURE);
-	}
+    perror("socket creation failed");
+    exit(EXIT_FAILURE);
+  }
 
   memset(&servaddr, 0, sizeof(servaddr));
   servaddr.sin_family = AF_INET;
@@ -22,15 +23,17 @@ int main() {
   servaddr.sin_addr.s_addr = INADDR_ANY;
 
   struct timeval timeout;    
-	timeout.tv_sec = 0;
-	timeout.tv_usec = 500*1000;
-	if (setsockopt (sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof timeout) < 0)  
-		herror("setsockopt failed\n");
+  timeout.tv_sec = 0;
+  timeout.tv_usec = 500*1000;
+  if (setsockopt (sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof timeout) < 0)  
+    herror("setsockopt failed\n");
 
   int n, len;
   char* msg;
+  char hello[30] = "hello from client !";
   while(1) {
-    n = recvfrom(sockfd, (char*)msg, MAXLINE, NULL, (struct sockaddr *) &servaddr, &len);
+    sendto(sockfd, (char*) hello, sizeof(e), 0, (const struct sockaddr *) &servaddr, len);
+    n = recvfrom(sockfd, (char*) msg, MAXLINE, NULL, (struct sockaddr *) &servaddr, &len);
     printf("msg: %s\n", msg);
     sleep(1);
   }
